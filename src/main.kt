88d850d713a53745fb648395322f335d6de07b9c
Samer <samer_n91@hotmail.com>
2017-12-28 10:29:25 +0100

    enhanced timedOut solution

--- src/main.kt
+++ src/main.kt
@@ -1,60 +1,67 @@
 /**
  * Created by Samer on 25/12/2017.
  */
-import javafx.collections.transformation.SortedList
-import java.io.*
 import java.util.*
 
 fun main(args: Array<String>) {
+    val sc = Scanner(System.`in`)
+    val target = sc.nextInt()
+    val mainList = mutableListOf<MutableList<List<Int>>?>()
+    for (i in 0 until target)
+        mainList.add(null)
+    for (i in 1..sc.nextInt()) {
+        val nextInt = sc.nextInt()
+        if (nextInt <= target && nextInt != 0) {
+            mainList[nextInt] = mutableListOf()
+            mainList[nextInt]!!.add(listOf(nextInt))
+        }
+    }
 
     val currentTimeMillis = System.currentTimeMillis()
 
-    val sc = Scanner(System.`in`)
-    val target = sc.nextInt()
-
-    val mainList = mutableListOf<MutableMap<String, List<Int>>>()
-
-    for (i in 0 until target)
-        mainList.add(mutableMapOf())
-
-   for (i in 1..sc.nextInt()){
-        val nextInt = sc.nextInt()
-        if (nextInt <= target && nextInt != 0) {
-            mainList[nextInt].put(nextInt.toString(), listOf(nextInt))
-        }
-    }
     val checkPosib = checkPosib(mainList, target)
-    println(checkPosib.keys.joinToString("\n"))
+    println(checkPosib.joinToString("\n"))
     println(checkPosib.size)
     println(System.currentTimeMillis() - currentTimeMillis)
 }
 
-fun checkPosib(list: MutableList<MutableMap<String, List<Int>>>, target: Int): MutableMap<String, List<Int>> {
-    val res = mutableMapOf<String, List<Int>>()
+fun checkPosib(list: MutableList<MutableList<List<Int>>?>, target: Int): MutableSet<List<Int>> {
+    val res = mutableSetOf<List<Int>>()
     for (i in 1..target / 2) {
         if (i == 1) {
-            if (list[i].isEmpty()) {
+            if (list[i] == null) {
                 continue
             }
-        } else
-            list[i].putAll(checkPosib(list, i))
+        }
+        if (list[i] == null) {
+            list[i] = mutableListOf()
+            list[i]!!.addAll(checkPosib(list, i))
+        }
+
+        if (list[i]!!.isEmpty()) {
+            continue
+        }
         val complNum = target - i
-        list[complNum].putAll(checkPosib(list, complNum))
-        res.putAll(tryToCombine(list[i], list[complNum]))
+        if (list[complNum] == null) {
+            list[complNum] = mutableListOf()
+            list[complNum]!!.addAll(checkPosib(list, complNum))
+        }
+        res.addAll(tryToCombine(list[i]!!, list[complNum]!!))
     }
     return res
 }
 
-fun tryToCombine(map1: MutableMap<String, List<Int>>, map2: MutableMap<String, List<Int>>): MutableMap<String, List<Int>> {
-    if (map1.isEmpty() || map2.isEmpty()) {
-        return mutableMapOf()
+fun tryToCombine(l1: MutableList<List<Int>>, l2: MutableList<List<Int>>)
+        : MutableSet<List<Int>> {
+    if (l1.isEmpty() || l2.isEmpty()) {
+        return mutableSetOf()
     }
-    val res = mutableMapOf<String, List<Int>>()
-    for (m1 in map1)
-        for (m2 in map2) {
-            val newlist = m1.value + m2.value
+    val res = mutableSetOf<List<Int>>()
+    for (m1 in l1)
+        for (m2 in l2) {
+            val newlist = m1 + m2
             val sorted = newlist.sorted()
-            res.put(sorted.toString(), sorted)
+            res.add(sorted)
         }
     return res
 }
